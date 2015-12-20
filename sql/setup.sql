eb901e5904fabfb2d062bbf1f7f297e7d1339e2f
betahikaru <beta.hikaru2000+github@gmail.com>
2015-12-20 17:40:12 +0900

    Add sample-sql for setup

---
+++ sql/setup.sql
@@ -0,0 +1,7 @@
+CREATE TABLE users(
+  id char(20) primary key
+, name varchar(50)
+, email varchar(100)
+);
+INSERT INTO users values ("ichiro_suzuki", "鈴木 一朗", "ichiro-suzuki@site.com");
+INSERT INTO users values ("shiro_suzuki", "鈴木 四郎", "shiro-suzuki@site.com");
